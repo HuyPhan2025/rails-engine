165603030937bd303d10636681c9a6f769b8a22d
Huy Phan <huyphan520@gmail.com>
2023-03-27 22:26:13 -0500

    Create merchants_controller #index

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,1 +1,5 @@
-class API::V1::MerchantsController < ApplicationRecord
+class Api::V1::MerchantsController < ApplicationController
+  def index
+    render json: MerchantSerializer.new(Merchant.all)
+  end
+end
