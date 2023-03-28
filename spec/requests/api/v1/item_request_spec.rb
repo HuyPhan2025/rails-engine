a1683aebcd17afbb625e6d48c10b3994fc8711e2
Huy Phan <huyphan520@gmail.com>
2023-03-28 17:37:02 -0500

    Create test for getting one item and sad path

--- spec/requests/api/v1/item_request_spec.rb
+++ spec/requests/api/v1/item_request_spec.rb
@@ -19,4 +19,31 @@ RSpec.describe "Item API" do
       expect(parse[:data][0][:attributes][:name]).to eq(Item.first.name)
     end
   end
+
+  describe "#show" do
+    it "return one item" do
+      item = Item.first
+
+      get "/api/v1/items/#{item.id}"
+
+      expect(response).to be_successful 
+      
+      parse = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parse[:data].keys).to eq([:id, :type, :attributes])
+      expect(parse[:data][:attributes].size).to eq(4)
+      expect(parse[:data][:id]).to eq(item.id.to_s)
+      expect(parse[:data][:type]).to eq('item')
+      expect(parse[:data][:attributes][:name]).to eq(item.name)
+    end
+
+    it "should return 404" do
+      item = Item.first
+
+      get "/api/v1/items/abc"
+
+      expect(response).to have_http_status(404)
+      expect(JSON.parse(response.body)).to eq({"error"=>"Couldn't find Item with 'id'=abc"})
+    end
+  end
 end
