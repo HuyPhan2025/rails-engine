7bd2d0df020310fa259b81aabd3ed32856f2083a
Huy Phan <huyphan520@gmail.com>
2023-03-29 12:02:24 -0500

    Update items_controller and items_spec with create

--- spec/requests/api/v1/item_request_spec.rb
+++ spec/requests/api/v1/item_request_spec.rb
@@ -46,4 +46,40 @@ RSpec.describe "Item API" do
       expect(JSON.parse(response.body)).to eq({"error"=>"Couldn't find Item with 'id'=abc"})
     end
   end
+
+  describe "#create" do
+    it "can create an item" do
+      id = create(:merchant).id
+
+      item_params = ({
+                      name: 'Magic',
+                      description: 'Explanation for confusing Rails stuff',
+                      unit_price: 12345.99,
+                      merchant_id: id
+                    })
+
+      headers = { "CONTENT_TYPE" => "application/json" }          
+
+      post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+
+      created_item = Item.last
+
+      expect(response).to be_successful
+      expect(response).to have_http_status(201)
+      expect(created_item.name).to eq(item_params[:name])
+      expect(created_item.description).to eq(item_params[:description])
+      expect(created_item.unit_price).to eq(item_params[:unit_price])
+      expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+    end
+  end
+
+  # describe "#destroy" do
+  #   expect(Item.count).to eq(3)
+
+  #   delete "/api/v1/items/#{item.id}"
+
+  #   expect(response).to be_successful
+  #   expect(Item.count).to eq(2)
+
+  # end
 end
