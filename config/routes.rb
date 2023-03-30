35f483082f94c432de564fc3f3045617133ae023
Huy Phan <huyphan520@gmail.com>
2023-03-30 16:53:33 -0500

    Update routes
    Added "/merchants/find" to "merchants/search#show"

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
+  # get '/merchants/find', to: 'merchants/search#show'
   namespace :api do
     namespace :v1 do
+      get "/merchants/find", to: "merchants/search#show"
       resources :merchants, only: [:index, :show] do
         get "/items", to: "merchants/items#index"
       end
