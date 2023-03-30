032e04890194e99fc154c5f3dcfd7ba80c5f8861
Huy Phan <huyphan520@gmail.com>
2023-03-30 16:55:07 -0500

    Create spec and method for #search_merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,4 +9,22 @@ RSpec.describe Merchant, type: :model do
     it { should have_many :items}
     it { should have_many :invoices }
   end
+
+  describe "#class method" do
+    it "search for merchant, order alphabetically, and return the first merchant" do
+      merchant1 = create(:merchant, name: "Christy")
+      merchant2 = create(:merchant, name: "Christine")
+      merchant3 = create(:merchant, name: "Sue")
+
+      expect(Merchant.search_merchant("chris")).to eq(merchant2)
+    end
+
+    it "will return nil if no merchant is found" do
+      merchant1 = create(:merchant, name: "Christy")
+      merchant2 = create(:merchant, name: "Christine")
+      merchant3 = create(:merchant, name: "Sue")
+
+      expect(Merchant.search_merchant("abc")).to eq(nil)
+    end
+  end
 end
