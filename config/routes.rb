8bddb91a1cc95c95042cef5b96b866f8b756ef09
Huy Phan <huyphan520@gmail.com>
2023-03-30 23:27:26 -0500

    Update route
    /items/find_all to items/search#index

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "/merchants/find", to: "merchants/search#show"
+      get  "/items/find_all", to: "items/search#index"
       resources :merchants, only: [:index, :show] do
         get "/items", to: "merchants/items#index"
       end
-
       resources :items do
         get "/merchant", to: "items/merchants#show"
       end
